1590a4d32ef5bc184f86bd335a0b593fac66cdab
Guruprasad M <Guruprasad.M@shell.com>
2022-02-06 16:16:11 +0530

    Feb-06-22: Azure storage queue - teaser

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,56 +1,74 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import './App.css';
-import AboutMe from './components/about/aboutme';
-import BlogNov212021 from './components/blogs/blog-Nov21-2021';
-import BlogNov282021 from './components/blogs/blog-Nov28-2021';
-import BlogDec062021 from './components/blogs/blog-Dec06-2021';
-import Footer from './components/common/footer';
-import Header from './components/common/header';
+import "./App.css";
+import AboutMe from "./components/about/aboutme";
+import BlogNov212021 from "./components/blogs/blog-Nov21-2021";
+import BlogNov282021 from "./components/blogs/blog-Nov28-2021";
+import BlogDec062021 from "./components/blogs/blog-Dec06-2021";
+import Footer from "./components/common/footer";
+import Header from "./components/common/header";
+import BlogFeb062022 from "./components/blogs/blog-Feb06-2022";
 
 function App() {
-    return (
-        <div className="container-fluid">
-            <Header />
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-sm-2">
-                        <AboutMe />
-                    </div>
+  return (
+    <div className="container-fluid">
+      <Header />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-sm-2">
+            <AboutMe />
+          </div>
 
-                    <div className="col-sm-8">
-                        <Switch>
-                            <Route exact path="/">
-                                <BlogNov212021 />
-                            </Route>
-                            <Route path="/blogNov2821">
-                                <BlogNov282021 />
-                            </Route>
-                            <Route path="/blogDec0621">
-                                <BlogDec062021 />
-                            </Route>
-                        </Switch>
-                    </div>
+          <div className="col-sm-8">
+            <Switch>
+              <Route exact path="/">
+                <BlogFeb062022 />
+              </Route>
+              <Route path="/blogNov2121">
+                <BlogNov212021 />
+              </Route>
+              <Route path="/blogNov2821">
+                <BlogNov282021 />
+              </Route>
+              <Route path="/blogDec0621">
+                <BlogDec062021 />
+              </Route>
+              <Route path="/blogFeb0622">
+                <BlogFeb062022 />
+              </Route>
+            </Switch>
+          </div>
 
-                    <div className="col-sm-2">
-                        <h6 className="mt-4">Links to previous blogs</h6>
-                        <ul className="flex-column">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="/">Blog Nov21, 2021</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="/blogNov2821">Blog Nov28, 2021</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="/blogDec0621">Blog Dec06, 2021</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <Footer />
+          <div className="col-sm-2">
+            <h6 className="mt-4">Links to previous blogs</h6>
+            <ul className="flex-column">
+              <li className="nav-item">
+                <a className="nav-link active" href="/blogFeb0622">
+                  Blog Feb06, 2022
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/blogDec0621">
+                  Blog Dec06, 2021
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/blogNov2821">
+                  Blog Nov28, 2021
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/blogNov2121">
+                  Blog Nov21, 2021
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ blog/src/components/blogs/blog-Feb06-2022.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./blogs.css";
+
+const BlogFeb0622 = () => {
+  return (
+    <>
+      <h2>Blog 4 - Azure Queue Storage</h2>
+      <h6>Feb 06, 2022</h6>
+      <br />
+      <span>
+        <h2 className="background-yellow">Coming Soon !!!</h2>
+      </span>
+    </>
+  );
+};
+
+export default BlogFeb0622;
